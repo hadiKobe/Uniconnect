1cd0d5dc4afd60cdf740c85b9871e3b4005f154e
ShaitoMo <shaitomohammad695@gmail.com>
2025-04-21 17:45:40 +0300

    filter dynamic fix

--- app/api/posts/getPost/route.js
+++ app/api/posts/getPost/route.js
@@ -5,10 +5,10 @@ import { getServerSession } from "next-auth";
 import { authOptions } from "@/app/api/auth/[...nextauth]/route";
 
 export async function GET(request) {
-  // const session = await getServerSession(authOptions);
-  // if (!session) {
-  //   return Response.json({ error: "Unauthorized" }, { status: 401 });
-  // }
+   const session = await getServerSession(authOptions);
+if (!session) {
+     return Response.json({ error: "Unauthorized" }, { status: 401 });
+   }
 
   const { searchParams } = new URL(request.url);
   // filters : friends, major
@@ -23,8 +23,8 @@ export async function GET(request) {
   // };
 
   const filters = {
-    user_id: 14,
-    major: 'Psychology'
+    user_id: session.user.id,
+    major: session.user.major
   };
 
   // conditions
